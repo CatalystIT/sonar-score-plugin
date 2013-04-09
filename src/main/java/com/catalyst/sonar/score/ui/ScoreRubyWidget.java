045345b7797df1f051292e118db67f38ed1ec4db
Margi Womack <MWomack@CatalystITServices.com>
2013-04-09 23:26:09 +0000

    Updated the ScoreRubyWidget class.  It was out of date and j-unit tests were failing.

--- src/main/java/com/catalyst/sonar/score/ui/ScoreRubyWidget.java
+++ src/main/java/com/catalyst/sonar/score/ui/ScoreRubyWidget.java
@@ -4,30 +4,21 @@ import org.sonar.api.web.Description;
 import org.sonar.api.web.RubyRailsWidget;
 import org.sonar.api.web.UserRole;
 import org.sonar.api.web.WidgetCategory;
-import org.sonar.api.web.WidgetProperties;
-import org.sonar.api.web.WidgetProperty;
+
 
 @UserRole(UserRole.USER)
-@Description("How to use Ruby Widget Api")
-@WidgetCategory("Score")
-@WidgetProperties({
-	  @WidgetProperty(key = "input1",
-	    description = "This is a mandatory parameter",
-	    optional = false
-	  ),
-	  @WidgetProperty(key = "input2",
-	    description = "This is an optional parameter"
-	  )
-	})
+@Description("Add the point widget here!")
+@WidgetCategory("Points")
+
 
 public class ScoreRubyWidget extends AbstractRubyTemplate implements RubyRailsWidget{
 	
 	public String getId(){
-		return "score";
+		return "points";
 	}
 	
 	public String getTitle(){
-		return "Score";
+		return "Points";
 	}
 	
 	@Override
