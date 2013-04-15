dfaea44a651d5f48ec4252c4f4b18f7c3912fefc
Michael Fadhl <MFadhl@CatalystITServices.com>
2013-04-15 21:16:04 +0000

    Added Selenium to .pom file

--- src/test/java/Selenium/Score.java
+++ src/test/java/Selenium/Score.java
@@ -3,12 +3,24 @@ package Selenium;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Score {
+	private static WebDriver driver;
+	private static final String SONAR_HOME_PAGE_URL = "localhost:9000";
+	
+	@BeforeClass
+	public static void setup() {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
 
 	@Before
-	public void setUp() throws Exception {
+	public void beforetest() {
+		driver.navigate().to(SONAR_HOME_PAGE_URL);
 	}
 
 	@Test
