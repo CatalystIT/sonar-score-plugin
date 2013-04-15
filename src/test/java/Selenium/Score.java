45e848f7eb391d640db18089a936e0337d605260
Michael Fadhl <MFadhl@CatalystITServices.com>
2013-04-15 16:45:58 +0000

    Initial creation of the Selenium package and Score test class.

---
+++ src/test/java/Selenium/Score.java
@@ -0,0 +1,19 @@
+package Selenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Score {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
