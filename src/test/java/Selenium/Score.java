103b9c1fc7c5bed12036174cc3a981ee0c65c253
Michael Fadhl <MFadhl@CatalystITServices.com>
2013-04-15 22:03:30 +0000

    Small change in ScoreTest

--- src/test/java/Selenium/Score.java
+++ src/test/java/Selenium/Score.java
@@ -5,12 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Score {
 	private static WebDriver driver;
 	private static final String SONAR_HOME_PAGE_URL = "localhost:9000";
+	private static final String SONAR_DASHBOARD_INDEX_1_CSS = "a[href='/dashboard/index/1']";
+	private static final String SONAR_DASHBOARD_INDEX_1_STRING = "";
 	
 	@BeforeClass
 	public static void setup() {
@@ -21,6 +24,7 @@ public class Score {
 	@Before
 	public void beforetest() {
 		driver.navigate().to(SONAR_HOME_PAGE_URL);
+		driver.findElement(By.cssSelector(SONAR_DASHBOARD_INDEX_1_CSS)).click();
 	}
 
 	@Test
