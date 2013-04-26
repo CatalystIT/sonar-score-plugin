912800e071e43978e4ed9fca65179180d2e9ed38
Margi Womack <MWomack@CatalystITServices.com>
2013-04-26 20:37:23 +0000

    Formatted the project comparison widget.

--- src/main/java/com/catalyst/sonar/score/ui/ProjectComparisonWidget.java
+++ src/main/java/com/catalyst/sonar/score/ui/ProjectComparisonWidget.java
@@ -1,6 +1,5 @@
 package com.catalyst.sonar.score.ui;
 
-import static org.sonar.api.web.WidgetScope.GLOBAL;
 
 import org.sonar.api.web.AbstractRubyTemplate;
 import org.sonar.api.web.Description;
@@ -10,27 +9,24 @@ import org.sonar.api.web.WidgetCategory;
 import org.sonar.api.web.WidgetProperties;
 import org.sonar.api.web.WidgetProperty;
 import org.sonar.api.web.WidgetPropertyType;
-import org.sonar.api.web.WidgetScope;
-
 
 
 @UserRole(UserRole.USER)
 @Description("Compares point values for projeccts in the same project group.")
 @WidgetCategory("Score")
-@WidgetScope(GLOBAL)
 
 /**
  * Sets up the Enhanced Filter List widget's properties with keys, types and default values
  * 
  */
 @WidgetProperties({
-	 @WidgetProperty(key = ProjectComparisonWidget.PROJECT_COMPARISON_GROUP, type = WidgetPropertyType.STRING, optional = false),
-	  @WidgetProperty(key = ProjectComparisonWidget.DISPLAY_WIDGET_DESCRIPTION, type = WidgetPropertyType.BOOLEAN, defaultValue = "false")
+	 @WidgetProperty(key = ProjectComparisonWidget.PROJECT_COMPARISON_GROUP, type = WidgetPropertyType.STRING, optional = false)
+	 
 })
 
 public class ProjectComparisonWidget extends AbstractRubyTemplate implements RubyRailsWidget{
 	public static final String PROJECT_COMPARISON_GROUP = "groupId";
-	public static final String DISPLAY_WIDGET_DESCRIPTION = "projectComparison ";
+	
 	  /**
 		 * returns the widget's id
 		 * represents the "widget_key" in the widgets table
