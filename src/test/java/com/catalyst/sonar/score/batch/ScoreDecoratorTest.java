2fe05e70091e850cb0be7205f101f66b5c5c5fa2
James Dunn <JDunn@CatalystITServices.com>
2013-04-29 19:15:36 +0000

    ScoreDecoratorTest.java: ctrl+shift+f

--- src/test/java/com/catalyst/sonar/score/batch/ScoreDecoratorTest.java
+++ src/test/java/com/catalyst/sonar/score/batch/ScoreDecoratorTest.java
@@ -23,12 +23,12 @@ import com.google.common.collect.ImmutableList;
 
 /**
  * @author JDunn
- *
+ * 
  */
 public class ScoreDecoratorTest {
-//	abstract class MockResource extends Resource<Project> {		
-//	};
-	
+	// abstract class MockResource extends Resource<Project> {
+	// };
+
 	ScoreDecorator scoreDecorator;
 	Project project;
 	DecoratorContext mockContext;
@@ -56,34 +56,38 @@ public class ScoreDecoratorTest {
 		coverage = mock(Measure.class);
 		tangle = mock(Measure.class);
 		resource = mock(Resource.class);
-		when(resource.getQualifier()).thenReturn("NOT_" + Qualifiers.UNIT_TEST_FILE);
+		when(resource.getQualifier()).thenReturn(
+				"NOT_" + Qualifiers.UNIT_TEST_FILE);
 		utsResource = mock(Resource.class);
 		when(utsResource.getQualifier()).thenReturn(Qualifiers.UNIT_TEST_FILE);
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.batch.ScoreDecorator#usedMetrics()}.
+	 * Test method for
+	 * {@link com.catalyst.sonar.score.batch.ScoreDecorator#usedMetrics()}.
 	 */
 	@Test
 	public void testUsedMetrics() {
 		assertEquals(scoreDecorator.usedMetrics(), ImmutableList.of(
-				CoreMetrics.NCLOC,
-				CoreMetrics.VIOLATIONS_DENSITY,
+				CoreMetrics.NCLOC, CoreMetrics.VIOLATIONS_DENSITY,
 				CoreMetrics.PUBLIC_DOCUMENTED_API_DENSITY,
-				CoreMetrics.COVERAGE,
-				CoreMetrics.PACKAGE_TANGLE_INDEX ));
+				CoreMetrics.COVERAGE, CoreMetrics.PACKAGE_TANGLE_INDEX));
 	}
 
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.batch.ScoreDecorator#generatedMetrics()}.
+	 * Test method for
+	 * {@link com.catalyst.sonar.score.batch.ScoreDecorator#generatedMetrics()}.
 	 */
 	@Test
 	public void testGeneratedMetrics() {
-		assertEquals(scoreDecorator.generatedMetrics(), Arrays.asList(ScoreMetrics.POINTS));
+		assertEquals(scoreDecorator.generatedMetrics(),
+				Arrays.asList(ScoreMetrics.POINTS));
 	}
 
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.batch.ScoreDecorator#shouldExecuteOnProject(org.sonar.api.resources.Project)}.
+	 * Test method for
+	 * {@link com.catalyst.sonar.score.batch.ScoreDecorator#shouldExecuteOnProject(org.sonar.api.resources.Project)}
+	 * .
 	 */
 	@Test
 	public void testShouldExecuteOnProject() {
@@ -91,65 +95,86 @@ public class ScoreDecoratorTest {
 	}
 
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.batch.ScoreDecorator#shouldDecorateResource(org.sonar.api.resources.Resource, org.sonar.api.batch.DecoratorContext)}.
-	 * Tests for when the resource is a unit test resource.  It should return false.
+	 * Test method for
+	 * {@link com.catalyst.sonar.score.batch.ScoreDecorator#shouldDecorateResource(org.sonar.api.resources.Resource, org.sonar.api.batch.DecoratorContext)}
+	 * . Tests for when the resource is a unit test resource. It should return
+	 * false.
 	 */
 	@Test
 	public void testShouldDecorateResourceWhenResourceIsUnitTest() {
-		assertEquals(scoreDecorator.shouldDecorateResource(utsResource, mockContext2), false);//!ResourceUtils.isUnitTestClass(resource));
-	}
-	
-	/**
-	 * Test method for {@link com.catalyst.sonar.score.batch.ScoreDecorator#shouldDecorateResource(org.sonar.api.resources.Resource, org.sonar.api.batch.DecoratorContext)}.
-	 */
-	@Test
-	public void testShouldDecorateResource() {
-		assertEquals(scoreDecorator.shouldDecorateResource(utsResource, mockContext), false);//!ResourceUtils.isUnitTestClass(resource));
+		assertEquals(scoreDecorator.shouldDecorateResource(utsResource,
+				mockContext2), false);// !ResourceUtils.isUnitTestClass(resource));
 	}
 
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.batch.ScoreDecorator#decorate(org.sonar.api.resources.Resource, org.sonar.api.batch.DecoratorContext)}.
+	 * Test method for
+	 * {@link com.catalyst.sonar.score.batch.ScoreDecorator#shouldDecorateResource(org.sonar.api.resources.Resource, org.sonar.api.batch.DecoratorContext)}
+	 * .
+	 */
+	@Test
+	public void testShouldDecorateResource() {
+		assertEquals(
+				scoreDecorator.shouldDecorateResource(utsResource, mockContext),
+				false);// !ResourceUtils.isUnitTestClass(resource));
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.catalyst.sonar.score.batch.ScoreDecorator#decorate(org.sonar.api.resources.Resource, org.sonar.api.batch.DecoratorContext)}
+	 * .
 	 */
 	@Test
 	public void testDecorate() {
 		when(mockContext.getMeasure(CoreMetrics.NCLOC)).thenReturn(ncloc);
-		when(mockContext.getMeasure(CoreMetrics.VIOLATIONS_DENSITY)).thenReturn(violations);
-		when(mockContext.getMeasure(CoreMetrics.PUBLIC_DOCUMENTED_API_DENSITY)).thenReturn(api);
+		when(mockContext.getMeasure(CoreMetrics.VIOLATIONS_DENSITY))
+				.thenReturn(violations);
+		when(mockContext.getMeasure(CoreMetrics.PUBLIC_DOCUMENTED_API_DENSITY))
+				.thenReturn(api);
 		when(mockContext.getMeasure(CoreMetrics.COVERAGE)).thenReturn(coverage);
-		when(mockContext.getMeasure(CoreMetrics.PACKAGE_TANGLE_INDEX)).thenReturn(tangle);
+		when(mockContext.getMeasure(CoreMetrics.PACKAGE_TANGLE_INDEX))
+				.thenReturn(tangle);
 		when(ncloc.getValue()).thenReturn(1000.0);
 		when(violations.getValue()).thenReturn(95.0);
 		when(api.getValue()).thenReturn(80.0);
 		when(coverage.getValue()).thenReturn(88.0);
 		when(tangle.getValue()).thenReturn(0.0);
-		double points = Math.round((1000.0 *(95.0/PERCENT) * (80.0/PERCENT) * (88.0/PERCENT)) - (0.0*MAGNIFY_PACKAGE_TANGLE));
-		when(mockContext.saveMeasure(ScoreMetrics.POINTS, points)).thenReturn(mockContext);
+		double points = Math.round((1000.0 * (95.0 / PERCENT)
+				* (80.0 / PERCENT) * (88.0 / PERCENT))
+				- (0.0 * MAGNIFY_PACKAGE_TANGLE));
+		when(mockContext.saveMeasure(ScoreMetrics.POINTS, points)).thenReturn(
+				mockContext);
 		scoreDecorator.decorate(resource, mockContext);
 		verify(mockContext).saveMeasure(ScoreMetrics.POINTS, points);
 		scoreDecorator.decorate(utsResource, mockContext2);
 		verify(mockContext2, never()).saveMeasure(ScoreMetrics.POINTS, points);
-		
-		
+
 	}
-/**
- * Testing that a point's value cannot be negative.  The lowest score possible is zero.
- */
+
+	/**
+	 * Testing that a point's value cannot be negative. The lowest score
+	 * possible is zero.
+	 */
 	@Test
-public void testDecorateWithNegativePoints(){
-	
-	when(mockContext.getMeasure(CoreMetrics.NCLOC)).thenReturn(ncloc);
-	when(mockContext.getMeasure(CoreMetrics.VIOLATIONS_DENSITY)).thenReturn(violations);
-	when(mockContext.getMeasure(CoreMetrics.PUBLIC_DOCUMENTED_API_DENSITY)).thenReturn(api);
-	when(mockContext.getMeasure(CoreMetrics.COVERAGE)).thenReturn(coverage);
-	when(mockContext.getMeasure(CoreMetrics.PACKAGE_TANGLE_INDEX)).thenReturn(tangle);
-	when(ncloc.getValue()).thenReturn(1000.0);
-	when(violations.getValue()).thenReturn(95.0);
-	when(api.getValue()).thenReturn(80.0);
-	when(coverage.getValue()).thenReturn(88.0);
-	when(tangle.getValue()).thenReturn(500.0);
-	//points calculated in the getPointsValue method is -49,331 but will return zero
-	//because points be negative.  
-	assertEquals((int)scoreDecorator.getPointsValue(mockContext),(int)LOWEST_POINTS);
-}
+	public void testDecorateWithNegativePoints() {
+
+		when(mockContext.getMeasure(CoreMetrics.NCLOC)).thenReturn(ncloc);
+		when(mockContext.getMeasure(CoreMetrics.VIOLATIONS_DENSITY))
+				.thenReturn(violations);
+		when(mockContext.getMeasure(CoreMetrics.PUBLIC_DOCUMENTED_API_DENSITY))
+				.thenReturn(api);
+		when(mockContext.getMeasure(CoreMetrics.COVERAGE)).thenReturn(coverage);
+		when(mockContext.getMeasure(CoreMetrics.PACKAGE_TANGLE_INDEX))
+				.thenReturn(tangle);
+		when(ncloc.getValue()).thenReturn(1000.0);
+		when(violations.getValue()).thenReturn(95.0);
+		when(api.getValue()).thenReturn(80.0);
+		when(coverage.getValue()).thenReturn(88.0);
+		when(tangle.getValue()).thenReturn(500.0);
+		// points calculated in the getPointsValue method is -49,331 but will
+		// return zero
+		// because points be negative.
+		assertEquals((int) scoreDecorator.getPointsValue(mockContext),
+				(int) LOWEST_POINTS);
+	}
 
 }
