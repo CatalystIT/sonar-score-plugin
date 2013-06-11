dbb4ce2bb4ba8f79b750b1dda4c6b762d869c20d
Lackshmi Sajeev <LSajeev@CatalystITServices.com>
2013-06-11 23:50:34 +0000

    added equals and hashCode methods.

--- src/main/java/com/catalyst/sonar/score/batch/Criteria.java
+++ src/main/java/com/catalyst/sonar/score/batch/Criteria.java
@@ -1,38 +1,129 @@
 package com.catalyst.sonar.score.batch;
 
-import org.sonar.api.measures.Metric;
 
 public class Criteria {
 
-	Metric metric;
-	private double requiredAmt;
-	private int days;
+	private String metric;
+	private Double requiredAmt;
+	private Integer days;
 	
-	public Criteria(){
-		
+	public Criteria() {
+		// no args Constructor
 	}
-	
-	public Criteria(Metric metric, double requiredAmt, int days){
-		this.metric = metric;
+
+	/**
+	 * Constructor with params
+	 * 
+	 * @param metric
+	 * @param requiredAmt
+	 * @param days
+//	 */
+	public Criteria(String metric, double requiredAmt, int days) {
+		this.setMetric(metric);
 		this.setRequiredAmt(requiredAmt);
 		this.setDays(days);
 	}
 
-	public double getRequiredAmt() {
+	
+	/**
+	 * getter for required amt
+	 * 
+	 * @return
+	 */
+	public Double getRequiredAmt() {
 		return requiredAmt;
 	}
 
-	public void setRequiredAmt(double requiredAmt) {
-		this.requiredAmt = requiredAmt;
+	/**
+	 * setter fro required amt
+	 * 
+	 * @param requiredAmt2
+	 */
+	public void setRequiredAmt(double requiredAmt2) {
+		this.requiredAmt = requiredAmt2;
 	}
 
-	public int getDays() {
+	/**
+	 * getter for days
+	 * 
+	 * @return
+	 */
+	public Integer getDays() {
 		return days;
 	}
 
-	public void setDays(int days) {
+	/**
+	 * setter for days
+	 * 
+	 * @param days
+	 */
+	public void setDays(Integer days) {
 		this.days = days;
 	}
+
+	/**
+	 * getter for metric name
+	 * 
+	 * @return
+	 */
+	public String getMetric() {
+		return metric;
+	}
+
+	/**
+	 * setter for metric name
+	 * 
+	 * @param metric
+	 */
+	public void setMetric(String metric) {
+		this.metric = metric;
+	}
+
+	/**
+	 * Overrides the equals method
+	 */
+	@Override
+	public boolean equals(Object obj){
+		if(this == obj)
+			return true;
+		if(obj == null)
+			return false;
+		if(!getClass().equals(obj.getClass()))
+			return false;
+		Criteria criteria = (Criteria) obj;
+		if(metric == null){
+			if(criteria.metric != null)
+				return false;
+			else if(!metric.equals(criteria.metric))
+				return false;
+		}
+		if(requiredAmt == null){
+			if(!(criteria.requiredAmt == null))
+				return false;
+			else if(!(requiredAmt == criteria.requiredAmt))
+				return false;
+				
+		}
+		if(days == null){
+			if(!(criteria.days == null))
+				return false;
+			else if (!(days == criteria.days))
+				return false;
+		}
+		return true;
+	}
 	
-	
+	/**
+	 * overrides the hashCode
+	 */
+	@Override
+	public int hashCode(){
+		final int prime = 31;
+		int hash = 1;
+		
+		hash+= (prime * hash + ((metric == null) ? 0 : metric.hashCode()));
+		hash+= (prime * hash + ((requiredAmt == null) ? 0 : requiredAmt.hashCode()));
+		hash+= (prime * hash + ((days == null) ? 0 : days.hashCode()));
+		return hash;
+	}
 }
