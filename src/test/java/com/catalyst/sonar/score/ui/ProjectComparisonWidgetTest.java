862832f4fb1ffe7be6ddfba4d4e4c66e5b1fab43
Margi Womack <MWomack@CatalystITServices.com>
2013-06-18 21:00:57 +0000

    Committing j-unit tests for the TrophyWidget and added comments to the TrophyWidgetTest class.

--- src/test/java/com/catalyst/sonar/score/ui/ProjectComparisonWidgetTest.java
+++ src/test/java/com/catalyst/sonar/score/ui/ProjectComparisonWidgetTest.java
@@ -4,26 +4,41 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+/**
+ * 
+ * @author mwomack
+ *
+ */
 public class ProjectComparisonWidgetTest {
 	
 	private ProjectComparisonWidget testWidget;
 
+	/**
+	 * @throws java.lang.Exception
+	 * creates a ProjectComparisonWidget object
+	 */
 	@Before
 	public void setUp() throws Exception {
 		testWidget = new ProjectComparisonWidget();
 	}
 
+	/**
+	 * Testing that the ProjectComparisonWidget id can be retrieved
+	 */
 	@Test
 	public void testGetId() {
 		assertEquals("projectComparison", testWidget.getId());
 	}
-
+	/**
+	 * Testing that the ProjectComparisonWidget title can be retrieved
+	 */
 	@Test
 	public void testGetTitle() {
 		assertEquals("Project comparison widget", testWidget.getTitle());
 	}
-
+	/**
+	 * Testing that the ProjectComparisonWidget html.erb template path can be retrieved
+	 */
 	@Test
 	public void testGetTemplatePath() {
 		assertEquals("/score/projectComparison_widget.html.erb", testWidget.getTemplatePath());
