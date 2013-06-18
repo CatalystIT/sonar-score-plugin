b0f9b012785ff460ceb8f2465d2440fb36ace4e0
James Dunn <JDunn@CatalystITServices.com>
2013-06-18 15:54:11 +0000

    Refactored stuff in TrophySetTest.

--- src/test/java/com/catalyst/sonar/score/batch/trophies/TrophySetTest.java
+++ src/test/java/com/catalyst/sonar/score/batch/trophies/TrophySetTest.java
@@ -1,106 +1,84 @@
+/**
+ * 
+ */
 package com.catalyst.sonar.score.batch.trophies;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.HashSet;
-import java.util.List;
-
-import junit.framework.Assert;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.catalyst.sonar.score.batch.trophies.Criteria;
-import com.catalyst.sonar.score.batch.trophies.Trophy;
-import com.catalyst.sonar.score.batch.trophies.TrophySet;
-
+/**
+ * Test Class for TrohySet
+ *
+ */
 public class TrophySetTest {
 	
-	TrophySet testTrophySet;
-	Trophy testTrophy;
-	Trophy copyOfTestTrophy;
-	String trophyName = "Violation";
-	Trophy testTrophyOne;
-	String trophyNameOne = "GreatCode";
-	Trophy testTrophyTwo;
-	String trophyNameTwo = "GreatCode";
-	Criteria testCriteria;
-	String testMetric = "Violation";
-	double testRequiredAmt = 10;
-	int testDays = 14;
-	Criteria testCriteriaOne;
-	String testMetricOne = "Coverage";
-	double testRequiredAmtOne = 90;
-	int testDaysOne = 4;
-	Criteria testCriteriaTwo;
-	String testMetricTwo = "Points";
-	double testRequiredAmtTwo = 1000;
-	int testDaysTwo = 21;
-	List<Criteria> testList = new ArrayList<Criteria>();
+	private static final String NAME_IN_SET = "Trophy In Set";
+	private static final String NAME_NOT_IN_SET = "Trophy Not In Set";
+	private static final String NAME_ADD_TO_SET = "Trophy To Add To Set";
 	
-	
+	private TrophySet testSet;
+	private Trophy trophyInSet;
+	private Trophy trophyWithSameNameAsTrophyInSet;
+	private Trophy trophyNotInSet;
+	private Trophy trophyToAddToSet;
+	private Criteria criteria1;
+	private Criteria criteria2;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
+		trophyInSet = new Trophy(NAME_IN_SET);
+		trophyWithSameNameAsTrophyInSet = new Trophy(NAME_IN_SET);
 		
-		testTrophySet = new TrophySet();
-		testTrophy = new Trophy(trophyName);
-		copyOfTestTrophy = testTrophy;
-		testTrophy.addCriteria(testCriteria);
-		copyOfTestTrophy.addCriteria(testCriteriaOne);
-		testTrophyOne = new Trophy(trophyNameOne);
-		testTrophyTwo = new Trophy(trophyNameTwo);
-		testTrophyTwo.addCriteria(testCriteriaTwo);
-		testList.add(testCriteria);
-		testList.add(testCriteriaOne);
-		testCriteriaOne = new Criteria(testMetricOne, testRequiredAmtOne, testDaysOne);
-		testCriteriaTwo = new Criteria(testMetricTwo, testRequiredAmtTwo, testDaysTwo);
+		criteria1 = new Criteria("metric1", 90.0, 5);
+		criteria2 = new Criteria("metric2", 90.0, 5);
 		
+		trophyInSet.addCriteria(criteria1);
+		trophyWithSameNameAsTrophyInSet.addCriteria(criteria2);
 		
+		trophyToAddToSet = new Trophy(NAME_ADD_TO_SET);
+		trophyNotInSet = new Trophy(NAME_NOT_IN_SET);
+		
+		testSet = new TrophySet();
+		testSet.add(trophyInSet);
 	}
 
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.batch.trophies.TrophySet#add(com.catalyst.sonar.score.batch.trophies.Trophy)}.
+	 */
 	@Test
-	public void testIfSuperAddTrophyWorks() {
-		assertEquals(0, testTrophySet.size());
-		testTrophySet.add(testTrophy);
-		assertEquals(1, testTrophySet.size());
-		
+	public void testAddTrophy_SameNameAsTrophyAlreadyInSet() {
+		assertFalse(trophyInSet.getCriteria().contains(criteria2));
+		assertTrue(testSet.add(trophyWithSameNameAsTrophyInSet));
+		assertTrue(trophyInSet.getCriteria().contains(criteria2));
 	}
 	
-//	@Test
-//	public void testIfSameTrophyCanBeAddedAgain(){
-//		testTrophySet.add(testTrophy);
-//		assertEquals(1, testTrophySet.size());
-//		assertFalse("Same Trophy cannot be added", testTrophySet.add(copyOfTestTrophy));
-//		}
-//	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.batch.trophies.TrophySet#add(com.catalyst.sonar.score.batch.trophies.Trophy)}.
+	 */
 	@Test
-	public void testIfDifferentCriteriaWithSameTrophyNameCanBeAddedtoTrophy(){
-		testTrophyOne.addCriteria(testCriteriaOne);
-		testTrophyTwo.addCriteria(testCriteriaTwo);
-		testTrophySet.add(testTrophyOne);
-		testTrophySet.add(testTrophyTwo);
-		assertTrue(testTrophyOne.getCriteria().contains(testCriteriaTwo));
+	public void testAddTrophy_TrophyNotYetInSet() {
+		assertTrue(testSet.add(trophyToAddToSet));
 	}
 
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.batch.trophies.TrophySet#get(com.catalyst.sonar.score.batch.trophies.Trophy)}.
+	 */
 	@Test
-	public void testGet(){
-			testTrophySet.add(testTrophy);
-			assertTrue(testTrophy == testTrophySet.get(copyOfTestTrophy));
-		
-	}
-
-	@Test
-	public void testDeepCopy(){
-		
+	public void testGet_TrophyInSet() {
+		assertSame(trophyInSet, testSet.get(trophyWithSameNameAsTrophyInSet));
 	}
 	
-	@After
-	public void tearDown() throws Exception {
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.batch.trophies.TrophySet#get(com.catalyst.sonar.score.batch.trophies.Trophy)}.
+	 */
+	@Test
+	public void testGet_TrophyNotInSet() {
+		assertNull(testSet.get(trophyNotInSet));
 	}
-
-	
 
 }
