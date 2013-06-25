dc1f5eb3243d5dbc6be6e8fce5807d964ca03173
James Dunn <JDunn@CatalystITServices.com>
2013-06-25 00:21:09 +0000

    Adding package com.catalyst.sonar.score.api; adding Interface ScoreEntity.

---
+++ src/main/java/com/catalyst/sonar/score/api/ScoreEntity.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.catalyst.sonar.score.api;
+
+import org.sonar.api.BatchExtension;
+
+/**
+ * @author JDunn
+ * Provides a common parent for most classes in the package
+ * <code>com.catalyst.sonar.score.api</code>,
+ * extending {@link org.sonar.api.BatchExtension}.
+ *
+ */
+public interface ScoreEntity extends BatchExtension {
+
+}
