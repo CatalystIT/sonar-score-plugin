4e1dfa8982fde7a1f2c7e40880f8a8e4412d3171
James Dunn <JDunn@CatalystITServices.com>
2013-06-26 16:23:33 +0000

    Adding Award and AwardTest.

---
+++ src/main/java/com/catalyst/sonar/score/api/Award.java
@@ -0,0 +1,163 @@
+package com.catalyst.sonar.score.api;
+
+import java.util.Arrays;
+
+/**
+ * The <code>Award</code> class represents an award in Sonar for
+ * projects to earn, with a name and a <code>SearchableHashSet</code> of
+ * Criteria.
+ * @author JDunn
+ */
+public abstract class Award implements ScoreEntity {
+
+	public static final String UNNAMED_AWARD = "Unnamed Award";
+
+	private String name;
+	private SearchableHashSet<Criterion> criteria;
+	@SuppressWarnings("rawtypes")
+	private Group membersToInclude;
+	@SuppressWarnings("rawtypes")
+	private Group membersToExclude;
+
+	/**
+	 * Default Constructor, necessary for the SCORE plugin to work, calls
+	 * {@code this(UNNAMED_AWARD)}, preventing any fields from being {@code null}.
+	 */
+	public Award() {
+		this(UNNAMED_AWARD);
+	}
+
+	/**
+	 * Constructs an {@code Award}, setting the name to equal the
+	 * {@code String} name argument.
+	 * 
+	 * @param name
+	 */
+	@SuppressWarnings("rawtypes")
+	public Award(String name) {
+		this.name = name;
+		this.criteria = new SearchableHashSet<Criterion>();
+		this.membersToInclude = new Group();
+		this.membersToExclude = new Group();
+	}
+
+	/**
+	 * Adds a {@code Criterion} to the criteria.
+	 * 
+	 * @param criterion
+	 */
+	public boolean addCriterion(Criterion criterion) {
+		return this.criteria.add(criterion);
+	}
+	
+	/**
+	 * Adds {@code Criteria} to the criteria.
+	 * 
+	 * @param criterion
+	 */
+	public Award addCriteria(Criterion... criteria) {
+		this.criteria.addAll(Arrays.asList(criteria));
+		return this;
+	}
+
+	/**
+	 * Adds Members to include from receiving the {@code Award}.
+	 * Code elsewhere should include all members by default if this list is empty.
+	 * @param members
+	 * @return this
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Award addMembersToInclude(Member... members) {
+		this.membersToInclude.addAll(Arrays.asList(members));
+		return this;
+	}
+
+	/**
+	 * Adds Members to exclude from receiving the {@code Award}.
+	 * @param members
+	 * @return this
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Award addMembersToExclude(Member... members) {
+		this.membersToExclude.addAll(Arrays.asList(members));
+		return this;
+	}
+
+	/**
+	 * Overrides {@link java.lang.Object#equals(java.lang.Object)}, checking for
+	 * meaningful equality based on the name field.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj.getClass().equals(this.getClass()))) {
+			return false;
+		}
+		Award other = (Award) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Overrides {@link java.lang.Object#hashCode()}, calculating a hashCode
+	 * based on the name field.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 1;
+		hash += (prime * hash + ((name == null) ? 0 : name.hashCode()));
+		return hash;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 * @return this
+	 */
+	public Award setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	/**
+	 * @return an immutable copy of the criteria
+	 */
+	public SearchableHashSet<Criterion> getCriteria() {
+		return criteria.immutableCopy();
+	}
+	
+	/**
+	 * @return an immutable copy of the membersToInclude
+	 */
+	@SuppressWarnings("unchecked")
+	public SearchableHashSet<Criterion> getMembersToInclude() {
+		return membersToInclude.immutableCopy();
+	}
+	
+	/**
+	 * @return an immutable copy of the membersToExclude
+	 */
+	@SuppressWarnings("unchecked")
+	public SearchableHashSet<Criterion> getMembersToExclude() {
+		return membersToExclude.immutableCopy();
+	}
+
+}
---
+++ src/test/java/com/catalyst/sonar/score/api/AwardTest.java
@@ -0,0 +1,228 @@
+/**
+ * 
+ */
+package com.catalyst.sonar.score.api;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static com.catalyst.sonar.score.api.ApiTestConstants.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author JDunn
+ *
+ */
+public class AwardTest {
+	
+	private static final String AWARD_NAME = "Award One";
+	private static final String DIFFERENT_AWARD_NAME = "Award Two";
+	
+	private Award testAward;
+	private SearchableHashSet<Criterion> mockSet;
+	private SearchableHashSet<Criterion> realSet;
+	private Criterion criterion1;
+	private Criterion criterion2;
+	
+	private Object referenceToTestAward;
+	private Object nullAward;
+	private Object notAnAward;
+	private Award nullNameAward;
+	private Object anotherNullNameAward;
+	private Object differentNameAward;
+	private Object sameNameAward;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() throws Exception {
+		testAward = new TitleCup(AWARD_NAME);
+		mockSet = mock(SearchableHashSet.class);
+		realSet = new SearchableHashSet<Criterion>();
+		setField(testAward, "criteria", mockSet);
+		criterion1 = new Criterion(null, 1.1, 1);
+		criterion2 = new Criterion(null, 2.2, 2);
+		
+		referenceToTestAward = testAward;
+		nullAward = null;
+		notAnAward = "I'm actually a String.";
+		nullNameAward = new TitleCup(null);
+		anotherNullNameAward = new TitleCup(null);
+		differentNameAward = new TitleCup(DIFFERENT_AWARD_NAME);
+		sameNameAward = new TitleCup(AWARD_NAME);
+		((Award)sameNameAward).addCriterion(criterion1);
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#hashCode()}.
+	 * Asserts that two TitleCups with the same name have the same hashCode.
+	 */
+	@Test
+	public void testHashCode() {
+		assertEquals(new TitleCup(AWARD_NAME).hashCode(), testAward.hashCode());
+		assertEquals(new TitleCup(null).hashCode(), nullNameAward.hashCode());
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#Award()}.
+	 * Asserts that an <code>Award</code> created without a name parameter
+	 * will be given the name of the constant <code>UNNAMED_AWARD</code>. 
+	 */
+	@Test
+	public void testAward() {
+		assertEquals(Award.UNNAMED_AWARD, new TitleCup().getName());
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#Award(java.lang.String)}.
+	 */
+	@Test
+	public void testAwardString() {
+		assertEquals(AWARD_NAME, testAward.getName());
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#addCriterion(com.catalyst.sonar.score.api.Criterion)}.
+	 * Asserts that {@code testAward.addCriteria(criteria)} encapsulates calling {@code add()} on the set of Criteria within the set.
+	 */
+	@Test
+	public void testAddCriterion() {
+		when(mockSet.add(criterion1)).thenReturn(true);
+		assertTrue(testAward.addCriterion(criterion1));
+		when(mockSet.add(criterion1)).thenReturn(false);
+		assertFalse(testAward.addCriterion(criterion1));
+		setField(testAward, "criteria", realSet);
+		assertTrue(testAward.addCriterion(criterion1));
+		assertTrue(testAward.getCriteria().contains(criterion1));
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#addCriteria(com.catalyst.sonar.score.api.Criterion)}.
+	 * Asserts that {@code testAward.addCriteria(criteria)} adds the Criteria.
+	 */
+	@Test
+	public void testAddCriteria() {
+		setField(testAward, "criteria", realSet);
+		testAward.addCriteria(criterion1, criterion2);
+		assertTrue(testAward.getCriteria().contains(criterion1));
+		assertTrue(testAward.getCriteria().contains(criterion2));
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#addMembers(com.catalyst.sonar.score.api.Member)}.
+	 * Asserts that {@code testAward.addMembersToInclude(members...)} adds the members.
+	 */
+	@Test
+	public void testAddMembersToInclude() {
+		setField(testAward, "membersToInclude", GROUP1);
+		testAward.addMembersToInclude(USER1, USER2);
+		assertTrue(testAward.getMembersToInclude().contains(USER1));
+		assertTrue(testAward.getMembersToInclude().contains(USER2));
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#addCriteria(com.catalyst.sonar.score.api.Member)}.
+	 * Asserts that {@code testAward.addMembersToExclude(members...)} adds the members.
+	 */
+	@Test
+	public void testAddMembersToExclude() {
+		setField(testAward, "membersToExclude", GROUP2);
+		testAward.addMembersToExclude(USER1, USER2);
+		assertTrue(testAward.getMembersToExclude().contains(USER1));
+		assertTrue(testAward.getMembersToExclude().contains(USER2));
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#equals(java.lang.Object)}.
+	 * Asserts that two <code>Award</code>s are equal when they are the same <code>Award</code> in memory.
+	 */
+	@Test
+	public void testEqualsObject_isSameObjectInMemory_equals() {
+		assertEquals(testAward, referenceToTestAward);
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#equals(java.lang.Object)}.
+	 * Asserts that two <code>Award</code>s are not equal when the <code>Object</code> is <code>null</code>.
+	 */
+	@Test
+	public void testEqualsObject_isNull_notEquals() {
+		assertNotEquals(testAward, nullAward);
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#equals(java.lang.Object)}.
+	 * Asserts that two "<code>Award</code>s" are not equal when the <code>Object</code> is
+	 * not an instance of <code>Award</code>.
+	 */
+	@Test
+	public void testEqualsObject_isNotAnAward_notEquals() {
+		assertNotEquals(testAward, notAnAward);
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#equals(java.lang.Object)}.
+	 * Asserts that two <code>Award</code>s are not equal when the Award has a <code>null</code>
+	 * name and the <code>Object</code> has a non-<code>null</code> name.
+	 */
+	@Test
+	public void testEqualsObject_hasNonNullNameWhenAwardHasNullName_notEquals() {
+		assertNotEquals(nullNameAward, sameNameAward);
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#equals(java.lang.Object)}.
+	 * Asserts that two <code>Award</code>s are equal when they both have <code>null</code> names.
+	 */
+	@Test
+	public void testEqualsObject_hasNullNameWhenAwardHasNullName_equals() {
+		assertEquals(nullNameAward, anotherNullNameAward);
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#equals(java.lang.Object)}.
+	 * Asserts that two <code>Award</code>s are not equal when they each have different names.
+	 */
+	@Test
+	public void testEqualsObject_differentName_notEquals() {
+		assertNotEquals(testAward, differentNameAward);
+	}
+	
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#equals(java.lang.Object)}.
+	 * Asserts that two <code>Award</code>s are equal when they both have the same name.
+	 */
+	@Test
+	public void testEqualsObject_sameName_equals() {
+		assertEquals(testAward, sameNameAward);
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		assertEquals(AWARD_NAME, testAward.getName());
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#setName(java.lang.String)}.
+	 */
+	@Test
+	public void testSetName() {
+		testAward.setName(DIFFERENT_AWARD_NAME);
+		assertEquals(DIFFERENT_AWARD_NAME, testAward.getName());
+	}
+
+	/**
+	 * Test method for {@link com.catalyst.sonar.score.api.Award#getCriteria()}.
+	 */
+	@Test
+	public void testGetCriteriaSet() {
+		assertEquals(mockSet, getField(testAward, "criteria"));
+	}
+
+}
