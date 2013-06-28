59d6d1d822a07c76326cc754a667eba5a4b36f71
James Dunn <JDunn@CatalystITServices.com>
2013-06-28 17:03:49 +0000

    Cleaned up SearchableHashSetTest

--- src/test/java/com/catalyst/sonar/score/api/SearchableHashSetTest.java
+++ src/test/java/com/catalyst/sonar/score/api/SearchableHashSetTest.java
@@ -9,16 +9,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * @author James
- *
+ * Test Class for {@link SearchableHashSet}.
+ * 
+ * @author JDunn
  */
 public class SearchableHashSetTest {
-	
+
 	private SearchableHashSet<String> testSet;
 	private String testString = new String("Hi!");
 	private Object equalString = new String("Hi!");
 	private Object unequalString = new String("Yo!");
-	private String testString2 = new String("Hello!");
 
 	/**
 	 * @throws java.lang.Exception
@@ -30,19 +30,19 @@ public class SearchableHashSetTest {
 	}
 
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#get(java.lang.Object)}.
-	 * Asserts that the returned element is the same one as the one in the list when a different but
-	 * meaningfully equal object is used as the argument.
+	 * Test method for {@link SearchableHashSet#get(Object)}. Asserts that the
+	 * returned element is the same one as the one in the list when a different
+	 * but meaningfully equal object is used as the argument.
 	 */
 	@Test
 	public void testGet_isEqualToObjectInSet_sameObject() {
 		assertSame(testString, testSet.get(equalString));
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#get(java.lang.Object)}.
-	 * Asserts that the returned element is the same one as the one in the list when a different but
-	 * meaningfully equal object is used as the argument.
+	 * Test method for {@link SearchableHashSet#get(Object)}. Asserts that the
+	 * returned element is the same one as the one in the list when a different
+	 * but meaningfully equal object is used as the argument.
 	 */
 	@Test
 	public void testGet_isNotEqualToObjectInSet_null() {
@@ -50,30 +50,30 @@ public class SearchableHashSetTest {
 	}
 
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#add(java.lang.Object)}.
-	 * Asserts that null cannot be added.
+	 * Test method for {@link SearchableHashSet#add(Object)}. Asserts that null
+	 * cannot be added.
 	 */
 	@Test
 	public void testAddE_isNull_false() {
 		assertFalse(testSet.add(null));
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#add(java.lang.Object)}.
-	 * Asserts that equal object cannot be added.
+	 * Test method for {@link SearchableHashSet#add(Object)}. Asserts that equal
+	 * object cannot be added.
 	 */
 	@Test
 	public void testAddE_isEqualToObjectInSet_false() {
-		assertFalse(testSet.add((String)equalString));
+		assertFalse(testSet.add((String) equalString));
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#add(java.lang.Object)}.
-	 * Asserts that unequal object can be added.
+	 * Test method for {@link SearchableHashSet#add(Object)}. Asserts that
+	 * unequal object can be added.
 	 */
 	@Test
 	public void testAddE_isNotEqualToObjectInSet_true() {
-		assertTrue(testSet.add((String)unequalString));
+		assertTrue(testSet.add((String) unequalString));
 	}
 
 }
