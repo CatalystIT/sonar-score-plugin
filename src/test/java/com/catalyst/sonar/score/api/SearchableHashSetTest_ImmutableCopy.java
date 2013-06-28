b903ce8ba7cf176d88d2bcf09fe6f453a1383874
James Dunn <JDunn@CatalystITServices.com>
2013-06-28 17:06:48 +0000

    Cleaned up SearchableHashSetTest_ImmutableCopy

--- src/test/java/com/catalyst/sonar/score/api/SearchableHashSetTest_ImmutableCopy.java
+++ src/test/java/com/catalyst/sonar/score/api/SearchableHashSetTest_ImmutableCopy.java
@@ -11,7 +11,8 @@ import org.junit.Test;
 
 /**
  * Test Cases for all possible outcomes of
- * {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
+ * {@link SearchableHashSet#immutableCopy()}.
+ * 
  * @author JDunn
  */
 public class SearchableHashSetTest_ImmutableCopy {
@@ -30,90 +31,84 @@ public class SearchableHashSetTest_ImmutableCopy {
 	}
 
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that immutable copy of the testSet has the same members.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * immutable copy of the testSet has the same members.
 	 */
 	@Test
 	public void testImmutableCopy_AssertMembersEqual() {
 		assertEquals(testSet, testSet.immutableCopy());
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code contains()} does not throw an exception.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code contains()} does not throw an exception.
 	 */
 	@Test
 	public void testImmutableCopy_Contains_Works() {
 		assertEquals(testSet.contains(USER1), immutableCopy.contains(USER1));
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code get()} does not throw an exception.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code get()} does not throw an exception.
 	 */
 	@Test
 	public void testImmutableCopy_Get_Works() {
 		assertEquals(testSet.get(USER1), immutableCopy.get(USER1));
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code add()} throws an {@code UnsupportedOperationException}.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code add()} throws an {@link UnsupportedOperationException}.
 	 */
 	@Test(expected = UnsupportedOperationException.class)
 	public void testImmutableCopy_Add_ThrowsException() {
 		immutableCopy.add(USER3);
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code remove()} throws an {@code UnsupportedOperationException}.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code remove()} throws an {@link UnsupportedOperationException}.
 	 */
 	@Test(expected = UnsupportedOperationException.class)
 	public void testImmutableCopy_Remove_ThrowsException() {
 		immutableCopy.remove(USER2);
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code clear()} throws an {@code UnsupportedOperationException}.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code clear()} throws an {@link UnsupportedOperationException}.
 	 */
 	@Test(expected = UnsupportedOperationException.class)
 	public void testImmutableCopy_Clear_ThrowsException() {
 		immutableCopy.clear();
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code removeAll()} throws an {@code UnsupportedOperationException}.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code removeAll()} throws an {@link UnsupportedOperationException}.
 	 */
 	@Test(expected = UnsupportedOperationException.class)
 	public void testImmutableCopy_AddAll_ThrowsException() {
 		immutableCopy.addAll(testSet);
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code removeAll()} throws an {@code UnsupportedOperationException}.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code removeAll()} throws an {@link UnsupportedOperationException}.
 	 */
 	@Test(expected = UnsupportedOperationException.class)
 	public void testImmutableCopy_RemoveAll_ThrowsException() {
 		immutableCopy.removeAll(testSet);
 	}
-	
+
 	/**
-	 * Test method for {@link com.catalyst.sonar.score.api.SearchableHashSet#immutableCopy()}.
-	 * Asserts that {@code retainAll()} throws an {@code UnsupportedOperationException}.
+	 * Test method for {@link SearchableHashSet#immutableCopy()}. Asserts that
+	 * {@code retainAll()} throws an {@link UnsupportedOperationException}.
 	 */
 	@Test(expected = UnsupportedOperationException.class)
 	public void testImmutableCopy_RetainAll_ThrowsException() {
 		immutableCopy.retainAll(testSet);
 	}
-	
-	@Test(expected=UnsupportedOperationException.class)
-	public void testThis() {
-		throw new UnsupportedOperationException("hi");
-	}
-
 
 }
