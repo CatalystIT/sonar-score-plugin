9193ff50adfed8f7d692a4e63cf3076edc3324c1
James Dunn <JDunn@CatalystITServices.com>
2013-06-28 18:02:53 +0000

    Cleaned up Member

--- src/main/java/com/catalyst/sonar/score/api/Member.java
+++ src/main/java/com/catalyst/sonar/score/api/Member.java
@@ -4,29 +4,30 @@
 package com.catalyst.sonar.score.api;
 
 /**
- * A member can be added to a {@link com.catalyst.sonar.score.api.Group}.
+ * A member can be added to a {@link Group}.
+ * 
  * @author JDunn
- *
  */
 public interface Member extends ScoreEntity {
-	
+
 	/**
 	 * @return the name
 	 */
 	String getName();
-	
+
 	/**
 	 * @return the uniqueId
 	 */
 	String getUniqueId();
-	
+
 	/**
 	 * @return the description
 	 */
 	String getDescription();
-	
+
 	/**
-	 * @param description the description to set
+	 * @param description
+	 *            the description to set
 	 */
 	void setDescription(String description);
 
