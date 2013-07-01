27e92873f6584b920ecf9f9602e9ce118c6f2ab8
James Dunn <JDunn@CatalystITServices.com>
2013-07-01 15:25:01 +0000

    Adding ScoreEntityDao and ScoreEntityDaoTest.

---
+++ src/main/java/com/catalyst/sonar/score/dao/ScoreEntityDao.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package com.catalyst.sonar.score.dao;
+
+import org.sonar.api.database.DatabaseSession;
+import org.sonar.jpa.dao.BaseDao;
+
+import com.catalyst.sonar.score.api.ScoreEntity;
+import com.catalyst.sonar.score.api.SearchableHashSet;
+
+/**
+ * The {@link ScoreEntityDao} class defines methods, mostly abstract, that will
+ * work with a {@link ScoreEntity} of type {@code E} and the database.
+ * 
+ * @author James
+ *
+ * @param <E>
+ */
+public abstract class ScoreEntityDao<E extends ScoreEntity> extends BaseDao {
+
+	/**
+	 * Constructor with a parameter for the session to set the session.
+	 */
+	public ScoreEntityDao(DatabaseSession session) {
+		super(session);
+	}
+
+	/**
+	 * Returns a {@link ScoreEntity} from the database with the same uniqueId by
+	 * calling {@code get(entity.getUniqueId())}.
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	public E get(E entity) {
+		return this.get(entity.getUniqueId());
+	}
+
+	/**
+	 * Returns a {@link ScoreEntity} from the database with a name equal to the
+	 * String name argument.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public abstract E get(String uniqueId);
+
+	/**
+	 * Retrieves all the {@code ScoreEntiti}es of type {@code e} in the
+	 * database.
+	 * 
+	 * @return
+	 */
+	public abstract SearchableHashSet<E> getAll();
+
+	/**
+	 * Creates a {@link ScoreEntity} in the database. Returns {@code true} if
+	 * successful and {@code false} if not.
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	public abstract boolean create(E entity);
+
+	/**
+	 * Updates a {@link ScoreEntity} in the database. Returns {@code true} if
+	 * successful and {@code false} if not.
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	public abstract boolean update(E entity);
+
+}
---
+++ src/test/java/com/catalyst/sonar/score/dao/ScoreEntityDaoTest.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package com.catalyst.sonar.score.dao;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.database.DatabaseSession;
+
+import com.catalyst.sonar.score.api.ScoreEntity;
+import com.catalyst.sonar.score.api.SearchableHashSet;
+
+/**
+ * Test Class for {@link ScoreEntityDao}.
+ * 
+ * @author JDunn
+ */
+public class ScoreEntityDaoTest {
+	
+	private static final String UNIQUEID = "Unique!";
+	
+	private static class SEntity implements ScoreEntity {
+
+		@Override
+		public String getUniqueId() {
+			return UNIQUEID;
+		}
+		
+	}
+	private static final SEntity ENTITY = new ScoreEntityDaoTest.SEntity();
+	
+	private static class SEDao extends ScoreEntityDao<SEntity> {
+
+		public SEDao(DatabaseSession session) {
+			super(session);
+		}
+
+		public SEntity get(String uniqueId) {
+			if(uniqueId.equals(UNIQUEID)) {
+				return ENTITY;
+			}
+			return null;
+		}
+
+		public SearchableHashSet<SEntity> getAll() {
+			return null;
+		}
+
+		public boolean create(SEntity entity) {
+			return false;
+		}
+
+		@Override
+		public boolean update(SEntity entity) {
+			return false;
+		}
+		
+	}
+	
+	private DatabaseSession mockSession;
+	private ScoreEntityDao<SEntity> testDao;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		mockSession = mock(DatabaseSession.class);
+		testDao = new SEDao(mockSession);
+		
+	}
+
+	/**
+	 * Test method for {@link ScoreEntityDao#get(ScoreEntity)}.
+	 */
+	@Test
+	public void testGetE() {
+		assertEquals(ENTITY, testDao.get(ENTITY));
+	}
+
+}
