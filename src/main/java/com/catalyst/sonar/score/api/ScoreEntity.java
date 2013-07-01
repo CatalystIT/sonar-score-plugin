6329f1528ced35577ab466357adad825e76cf813
James Dunn <JDunn@CatalystITServices.com>
2013-07-01 15:28:08 +0000

    Added missing javadoc to method signature in ScoreEntity.

--- src/main/java/com/catalyst/sonar/score/api/ScoreEntity.java
+++ src/main/java/com/catalyst/sonar/score/api/ScoreEntity.java
@@ -6,17 +6,25 @@ package com.catalyst.sonar.score.api;
 import org.sonar.api.BatchExtension;
 
 /**
- * @author JDunn
  * Provides a common parent for most classes in the package
- * {@code com.catalyst.sonar.score.api},
- * extending {@link org.sonar.api.BatchExtension}.
- *
+ * {@code com.catalyst.sonar.score.api}, extending
+ * {@link org.sonar.api.BatchExtension}.
+ * 
+ * @author JDunn
  */
 public interface ScoreEntity extends BatchExtension {
-	
+
 	String RAWTYPE_WARNING = "rawtypes";
 	String UNCHECKED_WARNING = "unchecked";
-	
+
+	/**
+	 * Gets the field that functions as the uniqueId for this
+	 * {@link ScoreEntity}. The name of this field will vary from implementation
+	 * to implementation, and most likely the field will have its own getter as
+	 * well.
+	 * 
+	 * @return
+	 */
 	String getUniqueId();
 
 }
