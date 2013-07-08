5a9022ff3cf0ce86280843b3ce3bba50666b3d56
Margi Womack <MWomack@CatalystITServices.com>
2013-07-08 16:10:06 +0000

    committing a few changes to the trophies_controller and added a few views to let the user know when a successful trophy/title cup has been created.  Also added a back button to return back to the Awards Page after the a successful trophy/title cup creation.  Changed the layout of the Awards Page.  The form is now horizontal as opposed to vertical.  Getting ready to for the ability to add multiple criteria.

--- src/main/resources/org/sonar/ror/score/app/controllers/trophies_controller.rb
+++ src/main/resources/org/sonar/ror/score/app/controllers/trophies_controller.rb
@@ -8,17 +8,26 @@ class TrophiesController < ApplicationController
     # do not delete
   end
   
+  def success
+    # do not delete
+  end
+  
   def create 
     @trophy = Trophy.new(params[:trophy])
-    @trophy.saveTrophy()
-    redirect_to :controller => "trophies", :action => "index"          
+    if @trophy.saveTrophy() 
+      redirect_to :controller => "trophies", :action => "success"
+    else
+      redirect_to :controller => "trophies", :action => "index"
+    end
+  
   end 
 
-
-
-
-
-
-
+  def addCriteria
+    respond_to do |format|        
+      
+      format.js   { render :js => "$('#add_criteria_id').fadeOut()" }
+    end
+  end
+ 
 
 end
