99f78c0ab890eeecb4a199562a322211c4ffb3d2
James Dunn <JDunn@CatalystITServices.com>
2013-08-05 16:40:27 +0000

    AssignableScoreEntity: updated javadoc.

--- src/main/java/com/catalyst/sonar/score/api/AssignableScoreEntity.java
+++ src/main/java/com/catalyst/sonar/score/api/AssignableScoreEntity.java
@@ -4,8 +4,20 @@
 package com.catalyst.sonar.score.api;
 
 /**
+ * An {@code AssignableScoreEntity} is a {@link ScoreEntity} that can be
+ * assigned to another {@link Entity} by relating it via the Properties table in
+ * SonarQube's database.
+ * 
+ * <br/>
+ * Examples include:
+ * 
+ * <br/>
+ *  - {@link Award}
+ * 
+ * <br/>
+ *  - {@link ProjectGroup}
+ * 
  * @author JDunn
- *
  */
 public interface AssignableScoreEntity extends ScoreEntity {
 
