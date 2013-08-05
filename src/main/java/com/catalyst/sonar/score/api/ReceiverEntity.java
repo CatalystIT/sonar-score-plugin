71ba516935bb877df3915d8201fa01c5c9aa998b
James Dunn <JDunn@CatalystITServices.com>
2013-08-05 17:36:51 +0000

    Refactored: renamed ScoreProject to SonarProject

--- src/main/java/com/catalyst/sonar/score/api/ReceiverEntity.java
+++ src/main/java/com/catalyst/sonar/score/api/ReceiverEntity.java
@@ -4,8 +4,20 @@
 package com.catalyst.sonar.score.api;
 
 /**
+ * A {@code ReceiverEntity} is an {@link Entity} to which an
+ * {@link AssignableScoreEntity} can be assigned, by relating them via the
+ * Properties table in SonarQube's database.
+ * 
+ * <br/>
+ * Examples include:
+ * 
+ * <br/>
+ * - All classes implementing {@link Member}
+ * 
+ * <br/>
+ * - {@link ProjectGroup}
+ * 
  * @author JDunn
- *
  */
 public interface ReceiverEntity extends Entity {
 
